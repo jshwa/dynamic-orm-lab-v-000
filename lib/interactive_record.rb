c73d9ae9669224dc33bea43fc149b2f61af0b2f5
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-29 02:54:19 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,7 +37,7 @@ class InteractiveRecord
   end
 
   def self.find_by_name(name)
-    DB[:conn].execute("SELECT * FROM #{table_name_for_insert} WHERE name = ?", name)
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", name)
   end
 
   def self.find_by(attributes)
