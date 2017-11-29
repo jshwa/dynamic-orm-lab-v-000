e53bb95255c1241a719431a433350f904a6ab7e3
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-29 02:53:20 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,4 +35,12 @@ class InteractiveRecord
     DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
+
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{table_name_for_insert} WHERE name = ?", name)
+  end
+
+  def self.find_by(attributes)
+    DB[:conn].execute("SELECT * FROM #{table_name_for_insert}")
+  end
 end
