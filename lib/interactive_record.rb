e9421dc626a2dac73f98871b890ec4e19f0092b6
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-29 03:21:04 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,7 +43,7 @@ class InteractiveRecord
 
   def self.find_by(attributes)
     attributes.map do |col_name, value|
-      DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE ? = ?", "#{col_name}", value)
+      DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{col_name} = ?", value)
     end
   end
 end
