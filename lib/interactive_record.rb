1be2a421a6d97f6243b12ccc6a1dd366cd76d419
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-29 02:27:23 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,18 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    table_info = DB[:conn].execute("PRAGMA table_info(#{table_name})")
+    column_names = table_info.collect {|info| info.name}.flatten
+  end
+
+  def initialize(options = {})
+    self.column_names
+  end
 end
